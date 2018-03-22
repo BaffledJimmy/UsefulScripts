9729c4cb08f46d1815161d42a178ab68a9f77cd2
BaffledJimmy <tommac2005@gmail.com>
2018-03-22 15:37:32 +0000

    Update kali_setup.sh

--- kali_setup.sh
+++ kali_setup.sh
@@ -10,7 +10,9 @@ apt-get clean && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
 apt-get install python-setuptools
 easy_install pip
 pip install selenium
-apt-get install unrar unace rar unrar p7zip zip unzip p7zip-full p7zip-rar file-roller nfs-acl-tools freetds-dev -y
+apt-get install unrar jxplorer unace rdate krb5-user rar libssl1.0-dev libxml2-dev zlib1g-dev unrar p7zip zip unzip p7zip-full p7zip-rar file-roller nfs-acl-tools freetds-dev bloodhound cadaver csvtool bkhive bettercap creddump creddump7 exiftool fierce htop gobuster html2text ipcalc masscan mimikatz mingw-w64 ldap-utils rsh-client finger linux-exploit-suggester neo4j nishang nfs-common recon-ng rinetd samdum2 seclists smbclient sshuttle sshpass tree wce websploit unicorn-magic -y
+
+
 
 #msfconsole.rc
 #
@@ -31,6 +33,11 @@ git clone https://github.com/sandrogauci/sipvicious.git
 cd /opt
 git clone https://github.com/PowerShellEmpire/Empire.git
 #run setup manually.
+
+#SOAP-UI
+cd /opt
+wget -q https://s3.amazonaws.com/downloads.eviware/soapuios/5.4.0/SoapUI-x64-5.4.0.sh -o /tmp/soapui-x64-5.4.0.sh && \
+/tmp/soapui-x64-5.4.0.sh
 
 #Snarf
 #
@@ -63,7 +70,7 @@ chmod 755 /usr/bin/smbrelayx.py
 cp /opt/impacket/examples/goldenPac.py /usr/bin
 chmod 755 /usr/bin/goldenPac.py
 
-#payload autogeneration
+#Payload Generation
 #
 cd /opt
 git clone https://github.com/trustedsec/unicorn.git
@@ -74,7 +81,7 @@ echo 'python /opt/Veil-Evasion/Veil-Evasion.py -p python/meterpreter/rev_tcp -c 
 echo 'sleep 1' >> /payload_gen.sh
 echo 'mv -f /root/veil-output/compiled/payload.exe /var/www/FreshPayload.exe' >> /payload_gen.sh
 
-#msf resource scripts
+#msf Resource Scripts
 #
 echo "use multi/handler" >> /bounce
 echo "jobs -K" >> /bounce
@@ -112,6 +119,36 @@ git clone https://github.com/mubix/shellshocker-pocs.git
 
 # MAC ADDITIONS
 
+#Powersploit Stuff
+# apt-get install powersploit
+# PowerSploit is inside Kali Apt-get but the kali repo versiono doesnt include PowerView and PowerUp
+git clone https://github.com/PowerShellMafia/PowerSploit.git /opt/powersploit &> /dev/null
+if [ $? -eq 0 ]; then item_success "Installed PowerSploit"; else item_failure "Installed PowerSploit"; fi 
+
+# PowerSploit Extras
+cd /usr/share/powersploit/
+wget -q https://raw.githubusercontent.com/obscuresec/random/master/StartListener.py
+wget -q https://raw.githubusercontent.com/darkoperator/powershell_scripts/master/ps_encoder.py
+cd /opt/powersploit/
+wget -q https://raw.githubusercontent.com/obscuresec/random/master/StartListener.py
+wget -q https://raw.githubusercontent.com/darkoperator/powershell_scripts/master/ps_encoder.py
+
+# PSNmap
+cd /opt wget -q https://www.powershelladmin.com/w/images/3/3e/PSnmap.zip -O /tmp/psnmap.zip  && \
+unzip /tmp/psnmap.zip -d /opt/ &> /dev/null
+
+#Printer Exploits
+cd /opt
+git clone https://github.com/TheEvilSaint/praedasploit.git
+
+#DNSCat
+cd /opt
+git clone https://github.com/lukebaggett/dnscat2-powershell.git
+
+#DomainPasswordSpray
+cd /opt
+git clone https://github.com/dafthack/DomainPasswordSpray.git
+
 #Atomic-Red-Team
 cd /opt
 git clone https://github.com/redcanaryco/atomic-red-team.git
@@ -147,20 +184,37 @@ git clone https://github.com/OffensivePython/HeartLeak.git
 cd /opt
 git clone https://github.com/CoreSecurity/impacket.git
 cd impacket
+oython setup.py install
 pip install .
 pip install -r requirements_examples.txt
 python setup.py
+
+#SprayWMI
+cd /opt
+git clone git clone https://github.com/trustedsec/spraywmi.git
+mv /opt/spraywmi/wmis /opt/spraywmi/wmis.old
+cp /usr/bin/pth-wmis /opt/spraywmi/wmis
+
+#PowerShell PopUp 1
+cd /opt 
+git clone https://github.com/burntmacncheese/Powershell-PopupBox.git
+cd /opt
+https://github.com/cheetz/PowerShell_Popup.git
 
 #UnixWiz NBTScan
 cd /opt
 mkdir nbtscan
 cd nbtscan
-wget http://www.unixwiz.net/tools/nbtscan-1.0.35-redhat-linux
+wget --timeout=5 --waitretry=3 --tries=3 http://www.unixwiz.net/tools/nbtscan-1.0.35-redhat-linux
 chmod +x nbtscan-1.0.35-redhat-linux
 
 #LyncSniper
 cd /opt
 https://github.com/mdsecresearch/LyncSniper.git
+
+#DirSearch
+cd /opt
+git clone https://github.com/maurosoria/dirsearch.git
 
 #MailSniper
 cd /opt
@@ -200,6 +254,11 @@ cd /opt
 git clone https://github.com/Veil-Framework/PowerTools.git
 cp /opt/PowerTools/PowerUp/PowerUp.ps1 /var/www
 cp /opt/PowerTools/PowerView/powerview.ps1 /var/www
+
+#Iodine Windows Client
+cd /opt
+mkdir Iodine_Windows_Client && cd Iodine_Windows_Client
+wget --timeout=5 --waitretry=3 --tries=3 http://code.kryo.se/iodine/iodine-0.7.0-windows.zip
 
 #Pykek Setup
 cd /opt
