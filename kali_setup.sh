29c1b5495707f17fbba7139544e083b93cf0a5bd
BaffledJimmy <tommac2005@gmail.com>
2018-12-29 20:35:03 +0000

    Update kali_setup.sh

--- kali_setup.sh
+++ kali_setup.sh
@@ -14,6 +14,14 @@ easy_install pip
 pip install selenium
 apt-get install unrar jxplorer unace rdate krb5-user rar libssl1.0-dev libxml2-dev zlib1g-dev unrar p7zip zip unzip p7zip-full p7zip-rar file-roller nfs-acl-tools freetds-dev bloodhound cadaver csvtool bkhive bettercap creddump creddump7 exiftool fierce htop gobuster html2text ipcalc masscan mimikatz mingw-w64 ldap-utils rsh-client finger linux-exploit-suggester neo4j nishang nfs-common recon-ng rinetd samdum2 seclists smbclient sshuttle sshpass tree wce websploit unicorn-magic -y
 
+# Posh C2 & Supporting Modules
+cd /opt
+git clone https://github.com/nettitude/PoshC2_Python.git
+cd PoshC2_Python
+chmod +x ./Install.sh
+./Install.sh
+
+
 #msfconsole.rc
 #
 echo "spool /mylog.log" >> /msfconsole.rc
